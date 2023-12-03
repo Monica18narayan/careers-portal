40b19c6061643c9209776dc5b622ec6c033635f4
Monica N <monica.n@muraai.com>
2023-12-03 13:42:59 +0530

    doenload-pdf

--- src/app/apply-job/apply-job.component.ts
+++ src/app/apply-job/apply-job.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { JobsServicesService } from '../jobs-services.service';
-import { HttpClientModule } from '@angular/common/http';
-//import { JobsInterface } from '../jobsInterface';
 import Swal from 'sweetalert2';
-
-
-
+import html2canvas from 'html2canvas'; // Import html2canvas for capturing HTML content
+import jsPDF from 'jspdf'; // Import jsPDF
 
 @Component({
   selector: 'app-apply-job',
@@ -15,78 +12,100 @@ import Swal from 'sweetalert2';
   styleUrls: ['./apply-job.component.css']
 })
 export class ApplyJobComponent {
- 
 
+  title = 'Apply For This Job';
+  genders = ['Female', 'Male', 'cannot disclose'];
+  locs = ['Bangalore', 'Chennai'];
+  ApplyJob: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router, private jobservice: JobsServicesService) {
+    this.ApplyJob = this.fb.group({
+      firstname: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      lastname: ['', Validators.required],
+      gender: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      dob: ['', Validators.required],
+      phone: ['', Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
+      cursal: [null, [Validators.required]],
+      loc: ['', Validators.required],
+      skill: ['', Validators.required],
+      compname: ['', Validators.required],
+      jobtitle: ['', Validators.required],
+      jobloc: ['', Validators.required],
+      course: ['', Validators.required],
+      branch: ['', Validators.required],
+      college: ['', Validators.required],
+      colloc: ['', Validators.required],
+    });
+  }
 
-    title = 'Apply For This Job';
-    genders = ['Female','Male','cannot disclose']
-    locs=['Bangalore','Chennai']
-    ApplyJob: FormGroup;
-    JobsInterface: any;
-    //jobdata:JobsInterface[]=[];
+  get valid() {
+    return this.ApplyJob.controls;
+  }
 
- 
-
-    constructor(private fb: FormBuilder, private router:Router, private jobservice:JobsServicesService,private http:HttpClientModule) {
-
-        this.ApplyJob = this.fb.group({
-
-            firstname: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-            lastname: ['', Validators.required],
-            gender:['',Validators.required],
-            email: ['',Validators.compose([Validators.required, Validators.email])],
-            dob: ['', 
-              Validators.required,
-
-          ],
-            phone: ['', Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
-            cursal:[null, [Validators.required]],
-            loc:['', Validators.required],
-            skill:['', Validators.required],
-            compname:['', Validators.required],
-            jobtitle:['', Validators.required],
-            jobloc:['', Validators.required],
-            course:['', Validators.required],
-            branch:['', Validators.required],
-            college:['', Validators.required],
-            colloc:['', Validators.required],
-
+  onSubmit() {
+    this.jobservice.getJobsService(this.ApplyJob.value).subscribe(
+      (result: any) => {
+        console.log(result);
+        Swal.fire({
+          text: 'Job Applied Successfully!!',
+          icon: 'success',
+          iconColor: '#004D40',
+          confirmButtonColor: '#004D40',
+          customClass: {
+            popup: 'flex flex-col items-center',
+            title: 'text-center',
+            confirmButton: 'border-none focus:outline-none focus:border-none hover:border-none'
+          },
+          width: '26rem',
+        }).then(() => {
+          this.onDownloadForm(); // Call download function first
+          // Navigate to table page after the download (within the download function)
         });
-    }
-    get valid(){
-      return this.ApplyJob.controls;
-    }
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
+
+  onDownloadForm() {
+    if (this.ApplyJob.valid) {
+      const formData = this.ApplyJob.value;
+
+   
+      const doc = new jsPDF();
+
+      doc.setFontSize(12);
+      doc.setFont('helvetica', 'normal');
 
 
-    onSubmit() {
-      this.jobservice.getJobsService(this.ApplyJob.value).subscribe(
-        (result: any) => {
-          console.log(result);
-          Swal.fire({
-            text: 'Job Applied Successfully!!',
-            icon: 'success',
-            iconColor: '#004D40',
-            confirmButtonColor: '#004D40',
-             customClass: {
-              popup: 'flex flex-col items-center', // Display content in a column and center horizontally
-               // Setting height and width to 12px and adding margin-bottom
-              title: 'text-center', // Center the title text horizontally
-              confirmButton: 'border-none focus:outline-none focus:border-none hover:border-none' // Remove border and blue outline
-            },
-            width: '26rem',
-          }).then(() => {
-            this.router.navigate(['/table']);
-          });
-        },
-        (error) => {
-          console.error('Error:', error);
-        }
-      );
-    }
-     //console.log('interface valu--',this.jobdata);
-      
-   // }
-  
+      const pdfContent = `
+      Application Form Details
+      ----------------------------
+      First Name: ${formData.firstname}
+      Last Name: ${formData.lastname}
+      Gender: ${formData.gender}
+      Email: ${formData.email}
+      Phone: ${formData.phone}
+      Date of Birth: ${formData.dob}
+      Current Salary: ${formData.cursal}
+      Location: ${formData.loc}
+      Skills: ${formData.skill}
+      Company Name: ${formData.Compname}
+      Job Title: ${formData.jobtitle}
+      Job Location: ${formData.jobloc}
+      Course: ${formData.course}
+      Branch: ${formData.branch}
+      College: ${formData.college}
+      College Location: ${formData.colloc}
+    `;
 
+      doc.text(pdfContent, 10, 10);
+
+      doc.save('job_application.pdf');
+
+      this.router.navigate(['/table']);
+    }
+  }
 }
